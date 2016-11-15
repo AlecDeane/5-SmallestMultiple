e761cff40dd2cbbd0afbb1ff1047a7f288d1c7d0
Alec <Alec@192.168.0.11>
2016-11-14 16:00:31 -0800

    First build

--- src/pkg5/smallestmultiple/SmallestMultiple.java
+++ src/pkg5/smallestmultiple/SmallestMultiple.java
@@ -20,64 +20,37 @@ public class SmallestMultiple {
      * What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
      */
     public static void main(String[] args) {
-        //Only need this to work for primes as all over numbers up to 20 are comprised of these primes.
-        
-        ArrayList<Integer> primes = getPrimesToNumber(20); //20 for Project Euler
-        System.out.println(getLargestNum(primes));
-        
-        /*if(isMultipleOfAll(primes,9699690) ){
-            System.out.println("true");
-        } */
-        int lowestMultiple = getLowestMultiple(primes);
+        ArrayList<Integer> listNums = getListToNumber(20); //20 for Project Euler
+        System.out.println(getLargestNum(listNums));
+        int lowestMultiple = getLowestMultiple(listNums);
         System.out.println(lowestMultiple);
     }
     
-    public static ArrayList<Integer> getPrimesToNumber(int num){
+    public static ArrayList<Integer> getListToNumber(int num){
         /**
-        * This function will simply return the list of Prime numbers up
+        * This function will simply return the list of numbers up
         * to the given parameter 
         * 
-        * @param num The max number to which the list of Primes can go.
+        * @param num The max number to which the list of numbers can go.
         */
         ArrayList<Integer> listOfNumbers = new ArrayList<>();
-        ArrayList<Integer> listOfPrimes = new ArrayList<>();
         for(int i = 2;i<=num;i++){
             listOfNumbers.add(i);
         }
-        for(int number : listOfNumbers){
-            if(isPrime(number)){
-                listOfPrimes.add(number);
-            }
-        }
         return listOfNumbers;
-        //return listOfPrimes;
     }
     
-    public static boolean isPrime(int num){
-        /**
-        * This function will simply return true or false depending on whether
-        * the input "num" is a prime or not.
-        * 
-        * @param num number to be tested as Prime
-        */
-        
-        //This will loop through every
-        for(Long i = 1L;i<num;i++){
-            if(num % i == 0 && i != 1L && i != num){
-                //System.out.println(num + " is not a prime number.");
-                return false;
-            }
-        }
-        //System.out.println(num + " is a prime number!");
-        return true;
-    }
     
     public static int getLowestMultiple(ArrayList<Integer> listInt){
-        System.out.println("Entering getLowestMultiple");
+        /**
+        * This function will get the lowest common multiple of every integer in
+        * the supplied parameter list. 
+        * 
+        * @param listInt The supplied list.
+        */
         boolean notFound = true;
         int lowestMultiple = 0;
         int i = getLargestNum(listInt);
-        System.out.println("gLM: largestInt = " + i);
         int j = i;
         while(notFound){
                 if(isMultipleOfAll(listInt,i)){
@@ -91,6 +64,12 @@ public class SmallestMultiple {
     }
     
     public static int getLargestNum(ArrayList<Integer> nums){
+        /**
+        * This function will get the largest integer from the ArrayList
+        * of integers
+        * 
+        * @param nums The supplied list.
+        */
         int curMax = 0;
         for(int num : nums){
             if(num > curMax ){
@@ -101,12 +80,17 @@ public class SmallestMultiple {
     }
     
     public static boolean isMultipleOfAll(ArrayList<Integer> listInt,int numToCheck){
+        /**
+        * This function will check to see if numToCheck, the second parameter, is evenly
+        * divisible by every variable in ListInt.
+        * 
+        * @param ListInt The supplied ArrayList.
+        * @param numToCheck Number to be divided by each entry in ListInt.
+        */
         for(int num : listInt){
-            //does it divide evenly by each member of listInt?
             if(numToCheck % num != 0){
                 return false;
             }
-        
         }
         return true;
     }
